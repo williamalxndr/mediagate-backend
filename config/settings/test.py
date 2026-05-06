397acbcb7ef3c9ddd9524c3994b31dd612a49257
William Alexander <williamax2005@gmail.com>
2026-05-07 04:21:22 +0700

    use test db setting config for ci

---
+++ config/settings/test.py
@@ -0,0 +1,12 @@
+from .base import *  # noqa: F403
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "test.sqlite3",  # noqa: F405
+    }
+}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
