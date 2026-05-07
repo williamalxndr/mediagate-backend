22067826e09d8d56082e4c825749841085071020
William Alexander <williamax2005@gmail.com>
2026-05-08 02:11:02 +0700

    fix(config): trust localhost frontend origins for CSRF

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,6 +2,10 @@ from .base import *  # noqa: F403
 
 DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] += [  # noqa: F405
     "rest_framework.renderers.BrowsableAPIRenderer",
